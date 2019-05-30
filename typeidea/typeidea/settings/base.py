d7273eb260b8f00b9f17ce2e2633d95b6d138bc6
fsd <fsd.joris@hotmail.com>
2019-05-30 17:08:42 +0800

    create blog,config and comment models

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'Aisa/Shanghai'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
